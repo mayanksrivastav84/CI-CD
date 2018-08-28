f285c4e651b31f92fd0de43b24595b072c576eb1
mayanksrivastav84 <mayank.srivastav84@gmail.com>
2018-08-28 23:12:57 +0200

    Commit

--- Terraform/Demo.tf
+++ Terraform/Demo.tf
@@ -6,7 +6,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "private_keypath" {}
 variable "key_name" {
-  default = "FactorSenseKeys"
+  default = "FactorSense"
 }
 
 ########################################################################################################
