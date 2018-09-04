3d6f823dc008c0c27dd0794feaf5fbb0ce793bc7
mayanksrivastav84 <mayank.srivastav84@gmail.com>
2018-09-04 22:28:18 +0200

    Terraform script to deploy acloud guru instance.

---
+++ Terraform/acloudguru/t2_micro.tf
@@ -0,0 +1,77 @@
+#######################################################################################################
+#Security Variables
+########################################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key_name" {
+  default = "FactorSense"
+}
+
+
+########################################################################################################
+# Provider
+########################################################################################################
+
+provider "aws" {
+  access_key  = "${var.aws_access_key}"
+  secret_key  = "${var.aws_secret_key}"
+  region      = "us-east-1"
+}
+
+########################################################################################################
+# RESOURCES
+########################################################################################################
+
+#Security Groups#
+resource "aws_security_group" "nginx-sg" {
+  name = "nginx-sg"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  #SSH Access from Anywhere
+
+    ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  #SSH Access from Anywhere
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #SSH Access from Anywhere
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+#Instance
+
+resource "aws_instance" "nginx" {
+ami           = "${var.ami_id}"
+instance_type = "${var.instance_type}"
+subnet_id = "${aws_subnet.subnet1.id}"
+vpc_security_group_ids = ["${aws_security_group.nginx-sg.id}"]
+key_name      = "${var.key_name}"
+
+
+connection {
+  user        = "ec2-user"
+  private_key = "${file(var.private_key_path)}"
+}
+
+}
