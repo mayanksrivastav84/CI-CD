508974f25c4923ed86894e626b2ffb481ffd3396
mayank srivastava <mayank.srivastav84@gmail.com>
2018-12-08 23:31:52 +0100

    Add Virtual Network and Subnets
    Signed-off-by: mayank srivastava <mayank.srivastav84@gmail.com>

--- Terraform/azure_terraform/azure_rg/azure_resource_group.tf
+++ Terraform/azure_terraform/azure_rg/azure_resource_group.tf
@@ -22,23 +22,39 @@ resource "azurerm_resource_group" "factorsense" {
 
 }
 
-resource "azurerm_resource_group" "webapp_factorsense" {
-
-    name = "webapp_factorsense"
-    location = "West Europe"
-
-    tags {
-        environment = "Production"
-    }
-
-}
-
 ########################################################################################################
-# Create a Azure Recovery service vault 
+# Create a Azure virtual Network
 ########################################################################################################
-resource "azurerm_recovery_services_vault" "fsvault" {
-  name                = "fsvault"
+resource "azurerm_network_security_group" "fs_securityGroup" {
+  name                = "fs_securityGroup"
   location            = "${azurerm_resource_group.factorsense.location}"
   resource_group_name = "${azurerm_resource_group.factorsense.name}"
-  sku                 = "standard"
+}
+
+resource "azurerm_virtual_network" "fs_virtualnetwork" {
+  name                = "fs_virtualnetwork"
+  location            = "${azurerm_resource_group.factorsense.location}"
+  resource_group_name = "${azurerm_resource_group.factorsense.name}"
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet_finance"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet_risk"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet_treasury"
+    address_prefix = "10.0.3.0/24"
+    security_group = "${azurerm_network_security_group.fs_securityGroup.id}"
+  }
+
+  tags {
+    environment = "Production"
+  }
 }
