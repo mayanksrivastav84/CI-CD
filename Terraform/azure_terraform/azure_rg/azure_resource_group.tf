0fb5006557932c23673927b65bd21c41124b204c
mayank srivastava <mayank.srivastav84@gmail.com>
2018-12-09 19:46:40 +0100

    Create Azure Virtual Machine
    Signed-off-by: mayank srivastava <mayank.srivastav84@gmail.com>

--- Terraform/azure_terraform/azure_rg/azure_resource_group.tf
+++ Terraform/azure_terraform/azure_rg/azure_resource_group.tf
@@ -6,14 +6,17 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 
+variable "prefix" {
+  default = "fs"
+}
 
 ########################################################################################################
 # Create a Azure Resource Group on Azure
 ########################################################################################################
 
-resource "azurerm_resource_group" "factorsense" {
+resource "azurerm_resource_group" "rg_main" {
 
-    name = "rg_factorsense"
+    name = "${var.prefix}-resourcegroup"
     location = "West Europe"
 
     tags {
@@ -25,36 +28,82 @@ resource "azurerm_resource_group" "factorsense" {
 ########################################################################################################
 # Create a Azure virtual Network
 ########################################################################################################
-resource "azurerm_network_security_group" "fs_securityGroup" {
-  name                = "fs_securityGroup"
-  location            = "${azurerm_resource_group.factorsense.location}"
-  resource_group_name = "${azurerm_resource_group.factorsense.name}"
-}
-
-resource "azurerm_virtual_network" "fs_virtualnetwork" {
-  name                = "fs_virtualnetwork"
-  location            = "${azurerm_resource_group.factorsense.location}"
-  resource_group_name = "${azurerm_resource_group.factorsense.name}"
+resource "azurerm_virtual_network" "vn_main" {
+  name                = "${var.prefix}-network"
+  location            = "${azurerm_resource_group.rg_main.location}"
+  resource_group_name = "${azurerm_resource_group.rg_main.name}"
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
+}
 
-  subnet {
-    name           = "subnet_finance"
-    address_prefix = "10.0.1.0/24"
+resource "azurerm_subnet" "subnet_main" {
+  name                 = "main_subnet"
+  resource_group_name  = "${azurerm_resource_group.rg_main.name}"
+  virtual_network_name = "${azurerm_virtual_network.vn_main.id}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+
+resource "azurerm_subnet" "subnet_secondary" {
+  name                 = "main_subnet"
+  resource_group_name  = "${azurerm_resource_group.rg_main.name}"
+  virtual_network_name = "${azurerm_virtual_network.vn_main.id}"
+  address_prefix       = "10.0.3.0/24"
+}
+
+
+resource "azurerm_network_interface" "nw_interface_main" {
+  name                = "${var.prefix}-nic"
+  location            = "${azurerm_resource_group.rg_main.location}"
+  resource_group_name = "${azurerm_resource_group.rg_main.name}"
+
+  ip_configuration {
+    name                          = "IPConfiguration"
+    subnet_id                     = "${azurerm_subnet.subnet_main.id}"
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+
+########################################################################################################
+# Create a Azure virtual machines
+########################################################################################################
+
+resource "azurerm_virtual_machine" "main_vm" {
+  name                  =   "${var.prefix}-vm"
+  location              =   "${azurerm_resource_group.rg_main.location}"
+  resource_group_name   =   "${azurerm_resource_group.rg_main.name}"
+  network_interface_ids  =   ["${azurerm_network_interface.nw_interface_main.id}"]
+  vm_size               =    "Standard_B1s"
+
+  delete_os_disk_on_termination    =  true
+
+  delete_data_disks_on_termination =  true
+  
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
   }
 
-  subnet {
-    name           = "subnet_risk"
-    address_prefix = "10.0.2.0/24"
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
 
-  subnet {
-    name           = "subnet_treasury"
-    address_prefix = "10.0.3.0/24"
-    security_group = "${azurerm_network_security_group.fs_securityGroup.id}"
+  os_profile {
+    computer_name  = "${var.prefix}-vm"
+    admin_username = "admin"
+    admin_password = "Residency18"
   }
 
+   os_profile_linux_config {
+    disable_password_authentication = false
+  }
   tags {
-    environment = "Production"
+    environment = "staging"
   }
 }
